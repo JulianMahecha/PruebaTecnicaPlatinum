ff25e498885cbc773f66f4929e41a8163eb6398a
Julian Mahecha <jemahechaj@ut.edu.co>
2020-06-14 13:44:57 -0500

    initial server config methods

--- server/build/index.js
+++ server/build/index.js
@@ -1,2 +1,24 @@
 "use strict";
-console.log('funciona');
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+class Server {
+    /* constructor */
+    constructor() {
+        this.app = express_1.default();
+    }
+    /* config */
+    config() {
+        this.app.set('port', process.env.PORT || 3000); /* takes system server by default */
+    }
+    /* routes */
+    routes() {
+    }
+    /* server listen */
+    start() {
+        this.app.listen();
+    }
+}
+new Server();
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,24 @@
-console.log('funciona');
+import express, {Application} from 'express';
+
+class Server {
+
+        public app: Application;
+        /* constructor */
+        constructor(){
+            this.app = express();
+        }
+        /* config */
+        config(): void{
+            this.app.set('port', process.env.PORT || 3000); /* takes system server by default */
+        }
+        /* routes */
+        routes(): void{
+        
+        }
+        /* server listen */
+        start(): void{
+            this.app.listen();
+        }
+}
+
+new Server();
