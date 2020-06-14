56f22cfe3e2ecd35f96e80487e489eccae87ae0b
Julian Mahecha <jemahechaj@ut.edu.co>
2020-06-14 14:25:31 -0500

    setting database credentials

--- server/build/index.js
+++ server/build/index.js
@@ -7,6 +7,7 @@ const express_1 = __importDefault(require("express"));
 const indexRoutes_1 = __importDefault(require("./routes/indexRoutes"));
 const moviesRoutes_1 = __importDefault(require("./routes/moviesRoutes"));
 const morgan_1 = __importDefault(require("morgan"));
+const cors_1 = __importDefault(require("cors"));
 class Server {
     /* constructor */
     constructor() {
@@ -17,7 +18,10 @@ class Server {
     /* config */
     config() {
         this.app.set('port', process.env.PORT || 3000); /* takes system server by default */
-        this.app.use(morgan_1.default('dev'));
+        this.app.use(morgan_1.default('dev')); /* show client petitions */
+        this.app.use(cors_1.default()); /* enable frontend server comunication */
+        this.app.use(express_1.default.json()); /* enable json request format */
+        this.app.use(express_1.default.urlencoded({ extended: false }));
     }
     /* routes */
     routes() {
--- server/src/index.ts
+++ server/src/index.ts
@@ -14,16 +14,22 @@ class Server {
             this.config();
             this.routes();
         }
+
         /* config */
         config(): void{
             this.app.set('port', process.env.PORT || 3000); /* takes system server by default */
-            this.app.use(morgan('dev'));
+            this.app.use(morgan('dev')); /* show client petitions */
+            this.app.use(cors()); /* enable frontend server comunication */
+            this.app.use(express.json()); /* enable json request format */
+            this.app.use(express.urlencoded({extended: false}));
         }
+
         /* routes */
         routes(): void{
             this.app.use(indexRoutes);
             this.app.use('/api/movies', moviesRoutes);
         }
+
         /* server listen */
         start(): void{
             this.app.listen(this.app.get('port'), () =>{
