1f60be1e5b58fa1483e80ccd8a4a9c67a8df3287
Julian Mahecha <jemahechaj@ut.edu.co>
2020-06-15 12:54:03 -0500

    creating navigation

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavigationComponent } from './components/navigation/navigation.component';
+import { MovieFormComponent } from './components/movie-form/movie-form.component';
+import { MoviesListComponent } from './components/movies-list/movies-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavigationComponent
+    NavigationComponent,
+    MovieFormComponent,
+    MoviesListComponent
   ],
   imports: [
     BrowserModule,
